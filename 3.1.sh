0d564815438ba84f4b59a5688a7219f16088f9dd
QCSun1 <139152737+QCSun1@users.noreply.github.com>
2023-08-03 21:39:19 +0800

    Delete 3.1.sh

--- 3.1.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo -e "\033[31mENTER_TEXT\033[0m"
-echo -e "\033[32mENTER_TEXT\033[0m"
-echo -e "\033[33mENTER_TEXT\033[0m"
-echo -e "\033[34mENTER_TEXT\033[0m"
-echo -e "\033[35mENTER_TEXT\033[0m"
-echo -e "\033[36mENTER_TEXT\033[0m"
-echo -e "\033[37mENTER_TEXT\033[0m"
